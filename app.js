8f494ecac721c8d360a7570950015d3617d24d50
Julien Roche <roche.jul@gmail.com>
2026-09-19 17:49:45 +0200

    feat(step05): evolve JS for dynamicity

--- app.js
+++ app.js
@@ -7,12 +7,82 @@ function getBoxPosition(element) {
   };
 }
 
+function getMovingElementPositionToStaticElement(movingElement, staticElement) {
+  const box1Position = getBoxPosition(movingElement);
+  const box2Position = getBoxPosition(staticElement);
+
+  const left =
+    movingElement.clientWidth / 2 + box1Position.x <
+    box2Position.x + staticElement.clientWidth / 2;
+  const top =
+    movingElement.clientHeight / 2 + box1Position.y <
+    box2Position.y + staticElement.clientHeight / 2;
+
+  const above =
+    box2Position.x - staticElement.clientWidth <
+      box1Position.x + movingElement.clientWidth &&
+    box1Position.x + movingElement.clientWidth <
+      box2Position.x + staticElement.clientWidth * 2 &&
+    box1Position.y + movingElement.clientHeight < box2Position.y;
+
+  const below =
+    box2Position.x - staticElement.clientWidth <
+      box1Position.x + movingElement.clientWidth &&
+    box1Position.x + movingElement.clientWidth <
+      box2Position.x + staticElement.clientWidth * 2 &&
+    box2Position.y + staticElement.clientHeight <
+      box1Position.y + movingElement.clientHeight;
+
+  return { left, top, above, below };
+}
+
+function updateLinkClassSet(linkElement, { left, top }) {
+  linkElement.classList.remove(
+    'link--left-top',
+    'link--left-bottom',
+    'link--right-top',
+    'link--right-bottom',
+  );
+
+  if (left && top) {
+    linkElement.classList.add('link--left-top');
+  } else if (left && !top) {
+    linkElement.classList.add('link--left-bottom');
+  } else if (!left && top) {
+    linkElement.classList.add('link--right-top');
+  } else if (!left && !top) {
+    linkElement.classList.add('link--right-bottom');
+  }
+}
+
+function updateLink(linkElement, movingElement, staticElement) {
+  const position = getMovingElementPositionToStaticElement(
+    movingElement,
+    staticElement,
+  );
+
+  updateLinkClassSet(linkElement, position);
+}
+
 async function setup() {
   let lastCursorPositionForBoxMovement = null;
   let lastMovingBoxPosition = null;
   let movingBoxElement;
 
   const bodyElement = document.querySelector('.app__body');
+  const staticElement = document.querySelector('.box--second');
+  const movableBoxElements = document.querySelectorAll('.box--movable');
+  const linkElements = document.querySelectorAll('.link');
+  const elementsMapping = new Map();
+
+  movableBoxElements.forEach((boxElement, index) => {
+    elementsMapping.set(boxElement, {
+      box: boxElement,
+      link: linkElements[index],
+    });
+
+    updateLink(linkElements[index], boxElement, staticElement);
+  });
 
   bodyElement.addEventListener('pointermove', (event) => {
     console.log('Pointer moved in %s %s', event.offsetX, event.offsetY);
@@ -29,6 +99,11 @@ async function setup() {
       console.log('The box has moved from %s %s', diffX, diffY);
 
       movingBoxElement.style = `left: ${lastMovingBoxPosition.x + diffX}px; top: ${lastMovingBoxPosition.y + diffY}px;`;
+      updateLink(
+        elementsMapping.get(movingBoxElement).link,
+        movingBoxElement,
+        staticElement,
+      );
     }
   });
 
